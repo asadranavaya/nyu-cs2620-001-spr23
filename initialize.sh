fdfb2ffdf3972c78a1e293ffc416c6357f814671
Weiqi Hu <wh2168@nyu.edu>
2023-03-22 22:53:48 -0400

    add build script

--- initialize.sh
+++ initialize.sh
@@ -4,5 +4,11 @@ curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 sudo pip3 install requests
 sudo pip3 install pythonping
+sudo pip3 install wget
 cwd=$(pwd)
 sudo chmod -R 777 cwd
+sudo mkdir ~/test
+sudo cp -f test.txt ~/test/test.txt
+sudo apt-get install nginx -y
+sudo cp -f nginx.conf /etc/nginx/nginx.conf
+sudo /usr/sbin/nginx -s reload
