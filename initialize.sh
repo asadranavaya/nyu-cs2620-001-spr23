f0fbdd669253a850f459f4d9162e865956bfab84
Weiqi Hu <wh2168@nyu.edu>
2023-03-23 00:31:51 -0400

    add shell script to initialize

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,6 @@
-sudo yum install git
-yes
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
 sudo pip3 install requests
 sudo pip3 install pythonping
 sudo pip3 install wget
-cwd=$(pwd)
-sudo chmod -R 777 cwd
 sudo mkdir ~/test
 sudo cp -f test.txt ~/test/test.txt
 sudo apt-get install nginx -y
